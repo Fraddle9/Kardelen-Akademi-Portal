806712ecbce7082f7c59fdf26c6d4a29f5f2f679
Recep <areysoftware@gmail.com>
2024-06-29 01:47:17 +0300

    clerk middleware fix

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,11 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-export default clerkMiddleware({
+const isPublicRoute = createRouteMatcher(['/giris(.*)', '/kayit(.*)']);
+
+export default clerkMiddleware((auth, request) => {
+  if(!isPublicRoute(request)) {
+    auth().protect();
+  }
 });
 
 export const config = {
